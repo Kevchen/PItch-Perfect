5a2de2ef1f2e67737581895b7b47c675ea240d6f
Kevin <kevkschen@gmail.com>
2017-01-04 22:59:22 -0500

    init

---
+++ PItch Perfect/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PItch Perfect
+//
+//  Created by Kevin on 19/12/2016.
+//  Copyright © 2016 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed!")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
